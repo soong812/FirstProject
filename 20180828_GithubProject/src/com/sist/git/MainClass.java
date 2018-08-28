9340ae7e1fbe4ab1a692d33f0e9e78a130272821
P <P@sist60>
2018-08-28 15:28:29 +0900

    1111

--- 20180828_GithubProject/src/com/sist/git/MainClass.java
+++ 20180828_GithubProject/src/com/sist/git/MainClass.java
@@ -3,6 +3,10 @@ package com.sist.git;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
 	}
 
 }
